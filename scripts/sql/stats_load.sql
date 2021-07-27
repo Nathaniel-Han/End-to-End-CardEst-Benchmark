1830d3dae390bcaeb9be1339b799b6a095f617ca
yuxing <yuxinghn@gmail.com>
2021-07-27 19:49:42 +0800

    fix load scripts

--- scripts/sql/stats_load.sql
+++ scripts/sql/stats_load.sql
@@ -1,8 +1,8 @@
-\copy badges datasets/stats_simplified/badges.csv with CSV header;
-\copy comments datasets/stats_simplified/comments.csv with CSV header;
-\copy users datasets/stats_simplified/users.csv with CSV header;
-\copy tags datasets/stats_simplified/tags.csv with CSV header;
-\copy posts datasets/stats_simplified/posts.csv with CSV header;
-\copy votes datasets/stats_simplified/votes.csv with CSV header;
-\copy posthistory datasets/stats_simplified/postHistory.csv with CSV header;
-\copy postlinks datasets/stats_simplified/postLinks.csv with CSV header;
+\copy badges from 'datasets/stats_simplified/badges.csv' with CSV header;
+\copy comments from 'datasets/stats_simplified/comments.csv' with CSV header;
+\copy users from 'datasets/stats_simplified/users.csv' with CSV header;
+\copy tags from 'datasets/stats_simplified/tags.csv' with CSV header;
+\copy posts from 'datasets/stats_simplified/posts.csv' with CSV header;
+\copy votes from 'datasets/stats_simplified/votes.csv' with CSV header;
+\copy posthistory from 'datasets/stats_simplified/postHistory.csv' with CSV header;
+\copy postlinks from 'datasets/stats_simplified/postLinks.csv' with CSV header;
